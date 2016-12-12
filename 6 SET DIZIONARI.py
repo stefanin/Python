79e02d11ab451ede4aee65cd602dfeb7a949bfca
stefanin <stefano.cornelli@gmail.com>
2016-12-12 21:58:57 +0100

    fine 6° lezione

--- 6 SET DIZIONARI.py
+++ 6 SET DIZIONARI.py
@@ -33,5 +33,27 @@ print ("ce non è simmetrica seinverto i valori y_set - x_set ", y_set - x_set)
 print ("differenza simmetrica  y_set ^ x_set ",y_set," tra ",x_set, " ", y_set ^ x_set)
 print ("fozenset() congela l'insiame dato ",frozenset(y_set), "l'interpetre genererà un errore se si tenta di modificare il set")
 
+'''
+        DIZZIONARIO
+        qualsiasi tipo di dato associato ad una chiave che può essrere numerica o stringa con approccio misto
+
+'''
+
+nome_dict = {'nome':'homer','cognome':'simpson', 1:'ciao'}
+print (nome_dict)
+print()
+nome_dict= dict({1:'aa', 2:'bb'})
+print (nome_dict)
+print("nome_dict[1] restituisce il valore all' indice 1 '",nome_dict[1])
+print("nome_dict.get(1) restituisce il valore all' indice 1  e non dà errore se uno non esiste",nome_dict[1])
+nome_dict[1]= 123
+print("nome_dict[1]= 123 modifica il valore",nome_dict[1])
+indice='zio'
+nome_dict[indice]= "bella li"
+print("indice='zio' nome_dict[indice]= 123 modifica il valore ",nome_dict[indice])
+print("per rimuovere un valore chiave posso usare pop()  nome_dict.pop('zio')", nome_dict.pop('zio'),"quindi ", nome_dict )
+print("per rimuovere un valore chiave posso usare clear() cancella il dizzionario", nome_dict.clear() )
+print("per ordinare il dizzionario , K=list(nome_dict) K=sorted(K) ")
+
 
 
