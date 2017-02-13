28e686e32bd53379c3bddc0080b960beeb48d55a
stefanin <stefano.cornelli@gmail.com>
2017-02-13 21:25:58 +0100

    19° lezione FINE CORSO

--- 18 teatro.json.py
+++ 18 teatro.json.py
@@ -8,7 +8,41 @@ jsonStr = str(res.read().decode("utf-8"))
 data = json.loads(jsonStr)
 conn.close()
 print(data)
+print()
 print("Elenco")
 for a in data:
-    print(a+str(data[a]))
+    if a == "postidisponibili":
+        print("Posti Disponibili : ")
+        posti=data[a]
+        for b in posti:
+            print("                      "+b+" : "+str(posti[b]))
+    else:        
+        print(a+" : "+str(data[a]))
+#
+#
+# mw.php?id=1
+#
+#
+print("")
+print(" HTTP GET            :")
+conn = http.client.HTTPConnection("sclab.altervista.org", 80)
+conn.request("GET", "/mag/mw.php?id=1")
+res = conn.getresponse()
+print(res.read().decode("utf-8"))
+
+# https://github.com/stefanin/Python
+
+
+
+HOSTNAME = 'github.com'
+
+conn = http.client.HTTPSConnection(
+	HOSTNAME
+)
+conn.putrequest('GET', '/stefanin/Python')
+conn.endheaders()
+response = conn.getresponse()
+print (response.read())
+
+
 
